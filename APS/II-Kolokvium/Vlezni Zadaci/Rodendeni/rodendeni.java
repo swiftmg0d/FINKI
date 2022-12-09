ec33c24ff86c853bfb276cb3aae895dbd0e9b0b8
SwiftM <100959856+swiftmg0d@users.noreply.github.com>
2022-12-09 01:06:37 +0100

    bhreis :)

--- APS/II-Kolokvium/Vlezni Zadaci/Rodendeni/rodendeni.java
+++ APS/II-Kolokvium/Vlezni Zadaci/Rodendeni/rodendeni.java
@@ -3,15 +3,11 @@ package Rodendeni;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class rodendeni {
+public class Rodendeni {
 
     public static void main(String[] args) throws IOException {
         BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
