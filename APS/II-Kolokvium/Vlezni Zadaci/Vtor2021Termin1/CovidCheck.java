e36b8437dc649f94bd2f76a74c02ff4dc7f355af
SwiftM <100959856+swiftmg0d@users.noreply.github.com>
2022-12-12 03:28:52 +0100

    nuashonraithe bheris

--- APS/II-Kolokvium/Vlezni Zadaci/Vtor2021Termin1/CovidCheck.java
+++ APS/II-Kolokvium/Vlezni Zadaci/Vtor2021Termin1/CovidCheck.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class VTOR {
+public class CovidCheck {
     static String split(String line){
         String []spluted=line.split("\\s+");
         return spluted[0];
