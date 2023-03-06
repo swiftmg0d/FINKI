d78e4b81bf76fa191896169df3a9b2b893e7e12e
Zhivche <zhivche.gligorov@gmail.com>
2023-03-06 21:02:53 +0100

    baheris

---
+++ VP/Laboratory exercises/Exercise 3/Ordering a pizza/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ordering_a_pizza
+{
+    public partial class Ordering_a_pizza : Form
+    {
+        public Ordering_a_pizza()
+        {
+            InitializeComponent();
+        }
+    }
+}
