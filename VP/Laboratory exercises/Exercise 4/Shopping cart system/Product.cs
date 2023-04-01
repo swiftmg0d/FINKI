d42cc052cd805517a2607db62dcf4a8609e42d2c
Zhivche <zhivche.gligorov@gmail.com>
2023-04-02 01:25:24 +0200

    Add ex 4 - not full - 3/4

---
+++ VP/Laboratory exercises/Exercise 4/Shopping cart system/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_cart_system
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, string category, double price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+        public string tString(double times)
+        {
+            return $"{Name} {times} x {Price} = {times * Price}";
+        }
+    }
+}
